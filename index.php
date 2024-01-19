573d4090088d30a88962f6c544a044f864e07a6b
devdanh <149231528+devdanh@users.noreply.github.com>
2024-01-19 15:48:50 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+$a = 5;
+$b = 6;
+$tong = $a + $b;
+echo "Tong: $tong <br>";
+
+$hieu = $a - $b;
+echo "Hieu: $hieu <br>";
+
+$tich = $a * $b;
+echo "Tich: $tich <br>";
+
+$thuong = $a / $b;
+echo "Thuong: $thuong <br>";
+
+$sodu = $a % $b;
+echo "So du: $sodu <br>";
+
+$ss_bang = $a == $b;
+echo "So sanh: $ss_bang <br>";
+
+$ss_khac = $a != $b;
+echo "So sanh khac: $ss_khac <br>";
+
+$ss_lon_hon = $a > $b;
+echo "So sanh lon hon: $ss_lon_hon <br>";
+
+$ss_be_hon = $a < $b;
+echo "So sanh be hon: $ss_be_hon <br>";
+
+// $a = $a - 1;
+$a--;
+// $a = $a + $B;
+$a += $b;
+echo $a;
+?>  
